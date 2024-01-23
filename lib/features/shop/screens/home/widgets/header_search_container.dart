2a3a92c8c1bf6d21f8758cf437b3bd821a30aac9
Yaroslavgiv <yaroslavgiv31@gmail.com>
2024-01-23 08:53:51 +0300

    second

--- lib/features/shop/screens/home/widgets/header_search_container.dart
+++ lib/features/shop/screens/home/widgets/header_search_container.dart
@@ -8,7 +8,8 @@ import '../../../../../utils/helpers/helper_functions.dart';
 import '../../search/search.dart';
 
 class THeaderSearchContainer extends StatelessWidget {
-  const THeaderSearchContainer({super.key, this.showBackground = false, this.showBorder = false});
+  const THeaderSearchContainer(
+      {super.key, this.showBackground = false, this.showBorder = false});
 
   final bool showBackground, showBorder;
 
@@ -20,7 +21,7 @@ class THeaderSearchContainer extends StatelessWidget {
         width: TDeviceUtils.getScreenWidth(context),
         padding: const EdgeInsets.all(TSizes.md),
         decoration: BoxDecoration(
-          border: showBorder ? Border.all(color: TColors.grey) : null,
+            border: showBorder ? Border.all(color: TColors.grey) : null,
             borderRadius: BorderRadius.circular(TSizes.cardRadiusLg),
             color: showBackground
                 ? THelperFunctions.isDarkMode(context)
@@ -30,9 +31,13 @@ class THeaderSearchContainer extends StatelessWidget {
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            Icon(Iconsax.search_normal, color: THelperFunctions.isDarkMode(context) ? TColors.grey : TColors.darkGrey),
+            Icon(Iconsax.search_normal,
+                color: THelperFunctions.isDarkMode(context)
+                    ? TColors.grey
+                    : TColors.darkGrey),
             const SizedBox(width: TSizes.spaceBtwItems),
-            Text('Найти в Магазине', style: Theme.of(context).textTheme.bodySmall),
+            Text('Найти в Магазине',
+                style: Theme.of(context).textTheme.bodySmall),
           ],
         ),
       ),
